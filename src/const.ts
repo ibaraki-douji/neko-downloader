751e80da87b8a78bb3ebc9318721b6e8fb514eb2
Ibaraki Douji <daynesmorgan33@gmail.com>
2021-03-07 13:08:29 +0100

    feat: change puppeteer for headless linux

--- src/const.ts
+++ src/const.ts
@@ -2,6 +2,6 @@ export class Constants {
     static get BASE () {return "https://neko-sama.fr"}
     static get VOSTFR () {return this.BASE + "/animes-search-vostfr.json"}
     static get VF () {return this.BASE + "/animes-search-vf.json"}
-    static get PUPPETEER () {return {headless: true, timeout: 120000}}
+    static get PUPPETEER () {return {headless: true, timeout: 120000, args: ["--no-sandbox"]}}
     
 }

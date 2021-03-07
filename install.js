d32880de4057d5322c3ab0c5d2bf97fc762e0f1c
Ibaraki Douji <daynesmorgan33@gmail.com>
2021-03-07 13:01:01 +0100

    feat: linux ffmpeg auto install

--- install.js
+++ install.js
@@ -1,36 +1,17 @@
 const child = require("child_process");
-const ZIP = require("adm-zip");
-const https = require("https")
-const fs = require('fs');
-const Stream = require('stream').Transform;
-const { pipeline } = require('stream');
-const zlib = require('zlib');
-
 
 (() => {
-    if (process.platform == "win32") {
+    if (process.platform != "win32") {
         try {
-            child.execSync("mkdir ffmpeg");
-        } catch (e) {}
-        https.get("https://komine.fr/ffmpeg.zip", (res) => {
-            var data = [];
-            console.log("Downloading FFMPEG ZIP")
-
-            res.on('data', function (chunk) {
-                data.push(chunk);
-            });
-
-            res.on('end', function (x) {
-                console.log("Downloaded FFMPEG ZIP")
-                fs.writeFileSync(__dirname + "/ffmpeg.zip", Buffer.concat(data));
-                console.log("Extracting ZIP")
-                const zip = new ZIP(__dirname + "/ffmpeg.zip");
-                zip.extractEntryTo("ffmpeg-N-101394-g94bf3f90e9-win64-gpl-vulkan/bin/", __dirname + "/ffmpeg", false);
-                child.execSync("del \"" + __dirname + "\\ffmpeg.zip\"");
-                console.log("Exctracted ZIP")
-            });
-        });/* */
-
-       
+            child.execSync('command -v ffmpeg');
+            console.log("FFMPEG already installed");
+        } catch (e) {
+            try {
+                child.execSync("apt install -y ffmpeg");
+                console.log("FFMPEG installed");
+            } catch (r) {
+                console.log("FFMPEG not installed\nPlease run : sudo apt install -y ffmpeg");
+            }
+        }
     }
 })()
